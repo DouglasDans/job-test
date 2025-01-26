cb68e5468c31e35e7e0717af507d28cf0ad1f2c0
douglasdans <douglas.dans@gmail.com>
2025-01-26 14:59:56 -0300

    feat: adiciona integração com o banco de dados por meio do prismaORM dentro das actions do NextJS

--- src/actions/produto.action.ts
+++ src/actions/produto.action.ts
@@ -1,11 +1,32 @@
 'use server'
 
+import { PrismaClient } from "@prisma/client"
+import { redirect } from "next/navigation"
+
+const prisma = new PrismaClient()
+
 export async function createProduto(formData: FormData) {
   const produto : Produto = {
     nome: formData.get("nome") as string,
     valor: parseFloat(formData.get("valor") as string),
     descricao: formData.get("descricao") as string,
-    disponibilidadeVenda: formData.get("disponibilidade") == 'true' ? true : false
+    isDisponivel: formData.get("disponibilidade") == 'true' ? true : false
   }
-  console.log(produto);
+  
+  await prisma.produto.create({
+    data: {
+      nome: produto.nome,
+      valor: produto.valor,
+      descricao: produto.descricao,
+      isDisponivel: produto.isDisponivel
+    }
+  })
+
+  redirect("/")
+}
+
+export async function getAllProdutos(): Promise<Array<Produto>> {
+  return await prisma.produto.findMany({
+    orderBy: {valor : 'asc'}
+  })
 }
