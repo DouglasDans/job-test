f3ca1ddf6b1bb3e2ccb8bbcf68fa35b626ccd100
douglasdans <douglas.dans@gmail.com>
2025-01-26 10:45:13 -0300

    feat: adiciona página de listagem dos produtos

--- src/app/(listagem)/page.tsx
+++ src/app/(listagem)/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Button } from '@mui/joy'
+import styles from './page.module.scss'
+import { Fragment } from 'react'
+import ListItems from '@/components/list-items'
+import Link from 'next/link'
 
 type Props = {}
 
 export default function ListagemPage({ }: Props) {
+  const items: Array<Produto> = []
   return (
-    <div>ListagemPage</div>
+    <Fragment>
+      <div className={styles.titleContainer}>
+        <h1>Lista de Produtos</h1>
+        <Link href={'cadastro'}>
+          <Button>Adicionar Produtos</Button>
+        </Link>
+      </div>
+      <ListItems items={items} />
+    </Fragment>
   )
 }
